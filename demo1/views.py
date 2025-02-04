d2e83585f3a3a665910e4b7925f8ae0d8336b813
AbdulRahman <abdul.abdullah@inkitsolutions.com.au>
2025-02-04 17:33:53 +0530

    Made changes to the views

--- demo1/views.py
+++ demo1/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Hello, Django! from the main project repository")
+    return HttpResponse("Hello, Django! from the main project repository and now has been updated")
